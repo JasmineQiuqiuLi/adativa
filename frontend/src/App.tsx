9fba5a7c0c3c4ada766239f21c1a82741f6e7abc
JasmineQiuqiu <yl4780@tc.columbia.edu>
2026-04-30 19:03:24 -0400

    init backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div>
+      <section id="center">Section 1</section>
+      <div className="hero"> Hello</div>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </div>
+  )
+}
+
+export default App
